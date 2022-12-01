00236d516cad95bf7063ffbd75988787336193f8
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-12-01 13:32:04 -0500

    solve problem one

---
+++ day 1/problem-1-code.py
@@ -0,0 +1,29 @@
+with open("/Users/nicholasprice/Desktop/Development/code/advent-cal-22/day 1/problem-1-data.txt", 'r') as file:
+    cals = file.read()
+
+
+def cal_math(data):
+    nums = "".join(list(data)).splitlines()
+    elves = []
+    elf = []
+
+    for i in range(len(nums)):
+        if nums[i] != '':
+            elf.append(int(nums[i]))
+        else:
+            elves.append(elf)
+            elf = []
+
+    elves.append(elf)
+    
+    for i in range(len(elves)):
+        elves[i] = sum(elves[i])
+
+    print(max(elves))
+
+
+
+
+
+cal_math(cals)
+
