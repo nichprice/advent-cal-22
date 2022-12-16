07ba3c58f5f98875f9002f5e8ebc8e5eb8e8c48f
Nichprice <97806706+Nichprice@users.noreply.github.com>
2022-12-16 17:40:55 -0500

    day 10 problem 2

---
+++ day 10/problem-2.py
@@ -0,0 +1,36 @@
+with open("/Users/nicholasprice/Desktop/Development/code/advent-cal-22/day 10/data.txt", 'r') as file:
+    instr = [line.strip() for line in file]
+
+# with open("/Users/nicholasprice/Desktop/Development/code/advent-cal-22/day 10/dummy.txt", 'r') as file:
+#     dummy = [line.strip() for line in file]
+
+
+def radio(data):
+
+    steps = []
+    vals = []
+    
+    for i in range(len(data)):
+        if data[i][0] == 'a':
+            vals.append([1])
+            vals.append([1])
+        else:
+            vals.append([1])
+
+    j = 0
+    for i in range(len(data)):
+        if data[i][0] == 'a':
+            val = int(data[i][5:])
+            steps.append(val)
+            j += 2
+            for k in range(j, len(vals)):
+                vals[k][0] += val
+        else:
+            steps.append('x')
+            j += 1
+
+    print(f'steps =\n {steps} \n \nvals =\n {vals}')
+
+
+
+radio(instr)
